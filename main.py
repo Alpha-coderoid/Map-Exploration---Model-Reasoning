bfed8201fb949c7451fd7d49af5d553ea6d18da5
Alpha-coderoid <m.alphatronic@gmail.com>
2022-11-25 00:41:49 +0200

    added intial structure

---
+++ main.py
@@ -0,0 +1,19 @@
+from tabulate import tabulate
+map = [[' ', ' ', ' '], [' ', ' ', ' ']]
+# print(tabulate(map))
+for i in range(len(map)):
+    print(map[i])
+# print(map[0])
+# print(map[0][4])
+# print(map[1][1][1])
+size = input()
+while (True):
+    inp = input()
+    if inp == 'start':
+        break
+    inp = inp.split()
+    inp = [eval(i) for i in inp]
+    loc = (int(inp[0])*size)+int(inp[1])
+    map[loc][0] = inp[2]
+    for i in range(len(map)):
+        print(map[i])
